03c582f6cf48bd09f0154147b7b97c83b5e87b1f
ishar <isharjain19@gmail.com>
2023-04-13 22:06:22 +0530

    fix: eslint extraneous dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import eslint from 'vite-plugin-eslint';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslint()],
-})
+});
